ee568f4db5197ee07be6695b50bc76b058be3be1
meniNaft <mnaft404@gmail.com>
2024-07-25 17:22:15 +0300

    complete

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -5,10 +5,11 @@ namespace MyFriends.Controllers
 {
     public class CarController : Controller
     {
-        public IActionResult Model(string name, string year)
+        public IActionResult Model(string name, string year, string color)
         {
             CarModel s = new CarModel(name ?? "oopp", year ?? "oppp");
-            return View(s);
+            
+            return View(new ViewModel(s, color));
         }
     }
 }
---
+++ Models/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFriends.Models
+{
+    public class ViewModel
+    {
+        public CarModel Car { get; set; }
+        public string Color { get; set; }
+        public ViewModel(CarModel car, string color) 
+        {
+            Car = car;
+            Color = color;
+        }
+    }
+}
